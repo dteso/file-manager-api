249309638c18dca7b9d657a00964a4bee8731184
David Teso <dtesodev@gmail.com>
2022-07-30 12:54:03 +0200

    securize routes with token

--- src/main/java/com/kumonosu/filemanagerapi/configuration/SpringFoxConfig.java
+++ src/main/java/com/kumonosu/filemanagerapi/configuration/SpringFoxConfig.java
@@ -1,6 +1,8 @@
 package com.kumonosu.filemanagerapi.configuration;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,17 +10,25 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
 public class SpringFoxConfig {
+
+	public static final String AUTHORIZATION_HEADER = "Authorization";
+
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).select()
+		return new Docket(DocumentationType.SWAGGER_2).securityContexts(Arrays.asList(securityContext()))
+				.securitySchemes(Arrays.asList(apiKey())).select()
 				.apis(RequestHandlerSelectors.basePackage("com.kumonosu.filemanagerapi")).paths(PathSelectors.any())
 				.build().useDefaultResponseMessages(false).apiInfo(getApiInfo());
 	}
@@ -27,5 +37,20 @@ public class SpringFoxConfig {
 		return new ApiInfo("File Manager API", "Files management service", "1.0", "http://codmind.com/terms",
 				new Contact("Sigital CO", "https://codmind.com", "apis@codmind.com"), "LICENSE", "LICENSE URL",
 				Collections.emptyList());
+	}
+
+	private ApiKey apiKey() {
+		return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
+	}
+
+	private SecurityContext securityContext() {
+		return SecurityContext.builder().securityReferences(defaultAuth()).build();
+	}
+
+	private List<SecurityReference> defaultAuth() {
+		AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+		AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+		authorizationScopes[0] = authorizationScope;
+		return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
 	}
 }
